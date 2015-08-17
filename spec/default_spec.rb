5caefe6897a325b2b10f6376715c0b3af297f200
rich-murray <rich.j.murray@gmail.com>
2015-08-17 16:30:48 +0100

    Fix 'NameError No resource, method, or local variable named `default' for `Chef::Recipe "default"'

---
+++ spec/default_spec.rb
@@ -0,0 +1,17 @@
+require 'chefspec'
+
+# ChefSpec unit test for icaclient::default recipe
+describe 'icaclient::default' do    
+  let(:chef_run) do
+    ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe)
+  end
+
+  before do
+    stub_command('dpkg -l icaclient').and_return(1)
+  end
+  
+  it 'installs packages' do
+    expect(chef_run).to install_package('libxerces-c3.1')
+    expect(chef_run).to install_package('libwebkitgtk-1.0-0')    
+  end
+end
