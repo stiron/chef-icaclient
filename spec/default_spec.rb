0b708edc2553b525959da55f48771ff3312a4498
rich-murray <rich.j.murray@gmail.com>
2015-08-18 15:09:05 +0100

    Added selfservice.desktop to fix launcher error on Unity.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,7 +1,7 @@
 require 'chefspec'
 
 # ChefSpec unit test for icaclient::default recipe
-describe 'icaclient::default' do    
+describe 'icaclient::default' do
   let(:chef_run) do
     ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe)
   end
@@ -9,9 +9,13 @@ describe 'icaclient::default' do
   before do
     stub_command('dpkg -l icaclient').and_return(1)
   end
-  
+
   it 'installs packages' do
     expect(chef_run).to install_package('libxerces-c3.1')
-    expect(chef_run).to install_package('libwebkitgtk-1.0-0')    
+    expect(chef_run).to install_package('libwebkitgtk-1.0-0')
+  end
+
+  it 'creates selfservice.desktop file' do
+    expect(chef_run).to create_cookbook_file('/usr/share/applications/selfservice.desktop')
   end
 end
